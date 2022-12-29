1014d34af086eccf6dd9f978ece36636764b20b0
akleem <74227145+akleem-AA@users.noreply.github.com>
2022-12-30 00:12:05 +0530

    print star patten in js

---
+++ startPatten.js
@@ -0,0 +1,15 @@
+let a =''
+for (let i =1;i<5;i++){
+    a+="*"
+    console.log(a)
+}
+
+for (let i = 0 ; i<5;i++){
+    console.log('*'.repeat(i))
+}
+let st = ''
+for(let i = 0;i<5;i++){
+    for(let j= 0;j<i;j++){
+        console.log(st +="*")
+    }
+}
